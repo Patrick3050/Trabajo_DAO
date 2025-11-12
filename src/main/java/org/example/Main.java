309585191a304e151f54b933965420586493afad
Patrick3050 <patrickmarcelomurillochavez@gmail.com>
2025-11-12 12:05:59 +0100

    menu a medias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,45 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner t = new Scanner(System.in);
+        int opcion;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("""
+                    ------ Menu  -----
+                    1. Leer registro.
+                    2. Insertar registro.
+                    3. Actualizar registro.
+                    4. Borrar registro.
+                    5. Salir.
+                    ---------------------------
+                    Elige una opcion:""");
+
+            try {
+                opcion = t.nextInt();
+
+                switch (opcion) {
+                    case 1 -> System.out.println();
+                    case 2 -> System.out.println();
+                    case 3 -> System.out.println();
+                    case 4 -> System.out.println();
+                    case 5 -> System.out.println();
+                    default -> System.err.println("Opcion incorrecta.\n");
+                }
+
+            } catch (InputMismatchException e) {
+                System.err.println("Solo se acepta números, y no se aceptan números tan largos.\n");
+                t.next();
+                opcion = 0;
+            }
+            System.out.println();
+
+        } while (opcion != 5);
+
+        System.out.println("Ha terminado el menú.");
     }
 }
